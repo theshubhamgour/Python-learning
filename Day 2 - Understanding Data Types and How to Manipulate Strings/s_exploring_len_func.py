cd3a3144c4717f22983e92a4c90774bbfcd3e28f
Shubham Gour <iamtheshubhamgour@gmail.com>
2023-07-02 10:11:32 +0530

    Add the primitive datatype

---
+++ Day 2 - Understanding Data Types and How to Manipulate Strings/s_exploring_len_func.py
@@ -0,0 +1,8 @@
+len_name = len(input("Enter you name : "))
+
+# To Check the data type of the above 
+print(type(len_name))
+
+# Changing to String datatype
+change_char = str(len_name)
+print("Your name is : " +change_char+ " character long") 
