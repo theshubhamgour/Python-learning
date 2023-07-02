5ffc818cb12345f2120782dd13a11197f05dc181
Shubham Gour <iamtheshubhamgour@gmail.com>
2023-07-02 09:56:27 +0530

    Add the primitive datatype file

---
+++ Day 2 - Understanding Data Types and How to Manipulate Strings/ps_primitive_datatype.py
@@ -0,0 +1,19 @@
+#Primitive Datatypes in Python
+
+# String
+print("This is a String")
+
+# Integer
+number = 45
+# This is an integer
+print(number)
+
+# Float
+PI = 3.412
+# This is a float value
+print(PI)
+# Difference between integer and float is - float contains decimal value
+
+# Boolean
+Value = True # This maybe false
+print(Value)
