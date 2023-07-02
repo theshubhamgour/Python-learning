8a8789fb8aa6c2fd78bfce3eba3e6dc371aae9fb
Shubham Gour <iamtheshubhamgour@gmail.com>
2023-07-02 09:31:54 +0530

    Add the input.py file

---
+++ Day 1 - Working with Variables in Python to Manage Data/s_input.py
@@ -0,0 +1,4 @@
+# The below will take input from the user using the console and will print on  the screen
+# The vlaue is stored in the variable named name and then we made use of the variable in the print statement
+name = input("What's your name? ")
+print("Hello "+ name)
