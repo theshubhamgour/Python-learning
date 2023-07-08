bdb9c7b11e4525b39de1a8732e046c52f3380db0
Shubham Gour <iamtheshubhamgour@gmail.com>
2023-07-08 18:47:08 +0530

    Add if else file

---
+++ Day 3 - Control Flow and Logical Operators/s_if_else.py
@@ -0,0 +1,11 @@
+print("   __       _ _               ___                _            ")
+print("  /__\\ ___ | | | ___ _ __    / __\\___   __ _ ___| |_ ___ _ __ ")
+print(" / \\/// _ \\| | |/ _ \\ '__|  / /  / _ \\ / _` / __| __/ _ \\ '__|")
+print("/ _  \\ (_) | | |  __/ |    / /__| (_) | (_| \\__ \\ ||  __/ |   ")
+print("\\/ \\_/\\___/|_|_|\\___|_|    \\____/\\___/ \\__,_|___/\\__\\___|_|   ")
+print(" ")
+height = int(input("Enter Height in CM: "))
+if height >= 120:
+    print("Welcome to Roller Coaster")
+else:
+    print("Better Luck Next time")
