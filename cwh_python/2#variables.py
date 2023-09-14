1503fcf891d5b671218403f4bddcde4c3de31815
theshubhamgour <iamtheshubhamgour@gmail.com>
2023-09-14 09:21:01 +0530

    new

---
+++ cwh_python/2#variables.py
@@ -0,0 +1,19 @@
+var1 = "Hello World"
+print(var1)
+
+var2 = 4.6
+print(var2)
+
+# To check type of the variable
+print(type(var1))
+print(type(var2))
+
+# Typecasting
+var3 = "56"
+var4 = "67"
+
+print(var3 + var4)
+
+# This is typecasting 
+print(int(var3)+int(var4))
+
