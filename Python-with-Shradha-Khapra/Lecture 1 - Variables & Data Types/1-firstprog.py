eaa37134f478601dc1f80dc205efd6045ebb4a49
Shubham Gour <iamtheshubhamgour@gmail.com>
2024-04-29 08:36:41 +0530

    Added day-1 files initial

---
+++ Python-with-Shradha-Khapra/Lecture 1 - Variables & Data Types/1-firstprog.py
@@ -0,0 +1,21 @@
+
+#Here Print is a function which is used to print anything on the console
+print("Hello World")
+
+
+#Python Character-set
+'''
+Letters – A to Z, a to z
+Digits – 0 to 9
+Special Symbols - + - * / etc.
+Whitespaces – Blank Space, tab, carriage return, newline, formfeed
+Other characters – Python can process all ASCII and Unicode characters as part of data or literals
+'''
+
+# Variable - Name given to a memory location in program [Variables vary]
+name = "Shubham" 
+print(name)
+
+name = "Siddharth"
+print(name)
+
---
+++ Python-with-Shradha-Khapra/Lecture 1 - Variables & Data Types/2-data-types.py
@@ -0,0 +1,16 @@
+# Data Types
+'''
+Integers
+String
+Float
+Boolean
+'''
+
+name = "Barry" #String
+age = 23 #Integer
+weight = 55.99 #float
+
+# Printing in one line
+print("Hi my name is ", name ,"and my age is ",age)
+
+ 
