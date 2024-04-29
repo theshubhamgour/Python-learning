1cf3cf47e1157ff35de5ff877de0f95f20058052
Shubham Gour <iamtheshubhamgour@gmail.com>
2024-04-29 09:20:55 +0530

    Added conversion

---
+++ Python-with-Shradha-Khapra/Lecture 1 - Variables & Data Types/5-type-conversion.py
@@ -0,0 +1,12 @@
+# Type conversion -- Data type conversion automatically
+
+a, b = 1, 2.0
+sum = a + b
+print(sum)
+
+# Type Casting  -- Data type conversion manually
+a, b = 1, "2"
+c = int(b)
+sum = a + c
+
+print(sum)
