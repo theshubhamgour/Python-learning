0310015b79f3f556f31a6f7b8ba8d38b1845975f
Shubham Gour <iamtheshubhamgour@gmail.com>
2024-04-29 09:36:54 +0530

    Day-1 completed

---
+++ Python-with-Shradha-Khapra/Lecture 1 - Variables & Data Types/6-print-input.py
@@ -0,0 +1,11 @@
+# Input in Python
+
+# input( ) #result for input( ) is always a str
+
+# input( ) statement is used to accept values (using keyboard) from user
+
+name = input("Enter your name : ")
+print("Hi",name,"nice to meet you!")
+
+
+
---
+++ Python-with-Shradha-Khapra/Lecture 1 - Variables & Data Types/7-exercise.py
@@ -0,0 +1,23 @@
+# ----------------------------------------------------------#
+# Write a Program to input 2 numbers & print their sum.
+# num1 = int(input("Enter number 1 : "))
+# num2 = int(input("Enter number 2 : "))
+# print("The sum is: ",(num1 + num2))
+
+# ----------------------------------------------------------#
+# WAP to input side of a square & print its area.
+# side = float(input("Enter the side of the square : "))
+# print("Area of Square is : ", (side*side))
+
+# ----------------------------------------------------------#
+# WAP to input 2 floating point numbers & print their average.
+# number1 = float(input("Enter number 1 : "))
+# number2 = float(input("Enter number 2 : "))
+# average  = (number1 + number2 ) / 2
+# print("The average is : ",average)
+
+# ----------------------------------------------------------#
+# WAP to input 2 int numbers, a and b. Print True if a is greater than or equal to b. If not print False.
+# a = int(input("Enter number 1 : "))
+# b = int(input("Enter number 2 : "))
+# print(a>=b)
