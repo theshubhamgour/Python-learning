c4cb75aaf5a32ea1ac795087e667a81b2a4d7294
Shubham Gour <iamtheshubhamgour@gmail.com>
2024-05-01 12:21:02 +0530

    Day 3 : Completed

--- Python-with-Shradha-Khapra/Lecture 3 - List & Tuple in Python/3-practice.py
+++ Python-with-Shradha-Khapra/Lecture 3 - List & Tuple in Python/3-practice.py
@@ -30,6 +30,7 @@
 # students = ("C","D","A","A","B","B","A") #tuple
 # students = ["C","D","A","A","B","B","A"] #list
 # print("Total count of A in the tuple: ",students.count("A"))
+
 # students.sort()
 # print(students)
 
