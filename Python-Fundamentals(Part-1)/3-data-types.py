a0196608fae4a1ff0fa4328ffa72493dd87239ea
Shubham Gour <iamtheshubhamgour@gmail.com>
2026-01-09 11:12:47 +0530

    Add data-types

---
+++ Python-Fundamentals(Part-1)/3-data-types.py
@@ -0,0 +1,34 @@
+# Data types
+#Integers
+a = 10
+print("Value of a:",a)
+
+#Float
+b = 20.5
+print("Value of b:",b)
+
+#String
+c = "Hello, Python!"
+print("Value of c:",c)
+
+#Boolean
+d = True
+print("Value of d:",d)
+
+#List
+e = [1, 2, 3, 4, 5]
+print("Value of e:",e)
+
+#Tuple
+f = (10, 20, 30)
+print("Value of f:",f)
+
+#Dictionary
+g = {"name": "Alice", "age": 30}
+print("Value of g:",g)
+
+#Set
+h = {1, 2, 3, 4, 5}
+print("Value of h:",h)
+
+
