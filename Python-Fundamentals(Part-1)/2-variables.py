4d694b7a8febc15e20995a00010a72030df3047c
Shubham Gour <iamtheshubhamgour@gmail.com>
2026-01-09 11:08:17 +0530

    Add Variables

---
+++ Python-Fundamentals(Part-1)/2-variables.py
@@ -0,0 +1,13 @@
+# Variables : Containers for storing data values
+# In Python, variables are created when you assign a value to them.
+
+name = "Shubham"  # String variable
+age = 25          # Integer variable
+height = 5.9      # Float variable
+is_student = True # Boolean variable
+
+# You can print the variables to see their values
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student:", is_student)
