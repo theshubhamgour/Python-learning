d95756bb0572d9b2f7eb582335a2eb624757e97d
Shubham Gour <iamtheshubhamgour@gmail.com>
2026-01-09 10:36:27 +0530

    first-program

--- cwh_python/1#first.py
+++
@@ -1,9 +0,0 @@
-# This program prints the hello world program
-print('Hello world')
-
-
-# This is a comment used for documentation purposes
-'''
-This is a multi-line comment
-Used for Multi line comments 
-'''
