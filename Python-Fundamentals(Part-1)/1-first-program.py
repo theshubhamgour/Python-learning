db0e359342dd72ce82a0a32d293be3928073a4e2
Shubham Gour <iamtheshubhamgour@gmail.com>
2026-01-09 10:48:19 +0530

    first-programm added

--- Python-Fundamentals(Part-1)/1-first-program.py
+++ Python-Fundamentals(Part-1)/1-first-program.py
@@ -1,1 +1,9 @@
+# Print itself is a function that outputs text to the console
 print("Hello World!")
+
+# You can print multiple items in a single print statement
+print("Printing the output","in a single line")
+
+# You can also use escape characters to format the output
+print("To print the statements in a new line","\nThis is printed in a new line")
+
